2e7cd3a2a344035ca99e03cf97253ba47fb726fe
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-11-26 23:44:14 +0200

    polymorphism

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -10,7 +10,7 @@ int main()
 {
     Func* functions[2];
     functions[0] = new Func1(1, 1, 1);
-    functions[1] = new Func2(0, 1, 1);
+    functions[1] = new Func2(0, 1, 0);
 
     functions[0]->calculating();
     functions[1]->calculating();
--- Polymorphism/src/Func1.cpp
+++ Polymorphism/src/Func1.cpp
@@ -5,7 +5,20 @@
 
 using namespace std;
 
+
 void Func1::calculating(){
+    /*cout << "X: "; cin >> x;
+    cout << "Y: "; cin >> y;
+    cout << "Z: "; cin >> z;
+    */
+    if (x * y < 0) {
+        cout << "x * y < 0" << endl;
+        return;
+    }
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+    cout << "z: " << z << endl;
+
     cout << "(7 * x^4 + √3xy) = "  << (7 * pow(x, 4) + sqrt(3 * x * y)) << endl << endl;
 }
 
