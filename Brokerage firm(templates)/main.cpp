64c32843eb3811f4646a2694a80a0c4a0a0b52ec
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-12-06 02:04:03 +0200

    add menu

--- Brokerage firm(templates)/main.cpp
+++ Brokerage firm(templates)/main.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
+#include <string>
+
 #include "BrokerageFirm.h"
 
 using namespace std;
 
+
+void addBroker();
+void brokersInfo();
+
+
+BrokerageFirm firm;
+
 int main()
 {
-    /*Apartment apartment;
-    apartment.setParams(5, 85, "Mira 65");
-    apartment.outputApartmentInfo();*/
 
-    /*Client client("Max", "+38098 777 77 07", 5, 85, "Mira 90");
-    client.outputClientInfo();*/
-    BrokerageFirm firm;
-    firm.addBroker("Max");
-    firm.outputBrokersInfo();
+    cout << "Pavlyuk Vadim Ruslanovych" << endl;
+    cout << "V : 14" << endl;
+    cout << "L : B" << endl << endl;
+
+    bool cont = true;
+
+    do {
+        cout << "1. Add broker" << endl;
+        cout << "2. Add client" << endl;
+        cout << "3. Buy apartment" << endl;
+        cout << "4. Brokers info" << endl;
+        cout << "5. Exit" << endl;
+
+        string sc;
+        cin >> sc;
+
+        char c = sc[0];
+
+        switch(c) {
+            case '1' :  { addBroker(); break; }
+            case '2' :  { break; }
+            case '3' :  { break; }
+            case '4' :  { brokersInfo(); break; }
+            case '5' :  return 0;
+        }
+
+    } while (cont);
+
 
     return 0;
+}
+
+void addBroker() {
+    string name;
+    cout << "Input broker name : ";
+    cin >> name;
+    firm.addBroker(name);
+    system("clear");
+}
+
+void brokersInfo() {
+    firm.outputBrokersInfo();
 }
