467952fd44100ba0ef98509d991ca62d68f82a7b
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-12-08 15:49:59 +0200

    add class Display

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <GL/glew.h>
 
+#include "Display.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Display display(800, 600, "Hello World!");
+
+    while(!display.IsClosed()) {
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        display.Update();
+    }
+
     return 0;
 }
