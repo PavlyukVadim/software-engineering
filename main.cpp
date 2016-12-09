ed0acb1f989853cd4c2af6aca1e3a08e2535d3a6
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2016-12-09 17:43:28 +0200

    add classes Mesh, Vertex

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 
 #include "Display.h"
+#include "Mesh.h"
 
 using namespace std;
 
@@ -9,9 +10,20 @@ int main()
 {
     Display display(800, 600, "Hello World!");
 
+    Vertex vertices[] = {
+        Vertex(glm::vec3(-0.5, -0.5, 0)),
+        Vertex(glm::vec3(0, 0.5, 0)),
+        Vertex(glm::vec3(0.5, -0.5, 0))
+    };
+
+    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]) );
+
     while(!display.IsClosed()) {
         glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        mesh.Draw();
 
         display.Update();
     }
