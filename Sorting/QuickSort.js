fd04c5b726c5b3ac88f5c0d5968d8e71cfada165
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-15 23:49:03 +0200

    partition

--- Sorting/QuickSort.js
+++ Sorting/QuickSort.js
@@ -9,5 +9,23 @@ function quickSort(arr, p, r) {
 	return arr;
 }
 
+function partition(arr, p, r) {
+	let bearingElement = arr[r];
+	let i = p - 1;
+	for (let j = p; j < r; j++) {
+		if ( arr[j] <= bearingElement) {
+			i++;
+			let tempElement = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tempElement;
+		}
+	}
+	let tempElement = arr[r];
+	arr[r] = arr[i + 1];
+	arr[i + 1] = tempElement;
+	console.log(arr);
+	return i + 1;  
+}
+
 let arr = [1, 5, 3, 7, 8, 10, 8, 3];
 console.log( quickSort(arr, 0, arr.length - 1) );
