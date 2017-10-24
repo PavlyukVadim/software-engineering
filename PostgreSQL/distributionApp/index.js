0b067bdf6c7e1754d2bc5bb872f4f6ecbead7be6
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-25 00:42:17 +0300

    setting static on express

--- PostgreSQL/distributionApp/index.js
+++ PostgreSQL/distributionApp/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use('/', express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
