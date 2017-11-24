887bfa7c59d36c7a71ad9d7b26fc9dd4c91d9e71
PavlyukVadim <front1@insfero.com>
2017-11-24 11:42:44 +0200

    fix test

--- C++/MemoryAllocator/main.cpp
+++ C++/MemoryAllocator/main.cpp
@@ -3,70 +3,53 @@
 
 using namespace std;
 
-void fillBlock(void *start, int size, int filler)
-{
-	for(int i = 0; i < size; i++)
-	{
-		*((int*)start + 1) = filler;
-	}
-}
-
-
 void test()
 {
-	cout << "test() started! \n";
-	const int n = 2000;
-	const int calls = 15;
-	const int bSize = 50;
+	const int n = 10000;
+	const int numberOfBlocks = 10;
+	const int bSize = 100;
 	Allocator alloc = Allocator(n);
 	void *curBlock;
-	void *mas[calls]; // has all user blocks
+	void *mas[numberOfBlocks]; // has all user blocks
 
-	for(int i = 0; i < calls; i++)
+	for(int i = 0; i < numberOfBlocks; i++)
 	{
 		curBlock = alloc.mem_alloc(bSize);
 		mas[i] = curBlock;
-		fillBlock(curBlock, 50, 170); //170(dec) = 1010 1010(bin)
 	}
 
-	cout << "alloc 15 blocks (length = 50) \n";
+	cout << "alloc: "  <<  numberOfBlocks << "blocks " << endl;
 	alloc.mem_dump();
 
-	cout << "min 3th el to 20\n"; // good
-	alloc.mem_realloc(mas[3], 20);
+	cout << "5 el to 50" << endl;
+	alloc.mem_realloc(mas[5], 50);
 	alloc.mem_dump();
 
-	cout << "max 3th el to 30\n"; // good
-	alloc.mem_realloc(mas[3], 30);
+	cout << "5th el to 80" << endl;
+	alloc.mem_realloc(mas[5], 80);
 	alloc.mem_dump();
 
-	cout << "max 3th el to 49\n";
-	alloc.mem_realloc(mas[3], 49);
+	cout << "5th el to 100" << endl;
+	alloc.mem_realloc(mas[5], 100);
 	alloc.mem_dump();
 
-	cout << "free 2th and 4th els \n";
-	alloc.mem_free(mas[2]);
+	cout << "free 4th and 6th els" << endl;
 	alloc.mem_free(mas[4]);
+	alloc.mem_free(mas[6]);
 	alloc.mem_dump();
 
-	cout << "...and max 3th el to 156 \n";
-	alloc.mem_realloc(mas[3], 156);
+	cout << "5th el to 310" << endl;
+	alloc.mem_realloc(mas[5], 310);
 	alloc.mem_dump();
 
-	cout << "min 2th el to 53 \n";
-	alloc.mem_realloc(mas[2], 53);
+	cout << "4th el to 103 \n" << endl;
+	alloc.mem_realloc(mas[4], 103);
 	alloc.mem_dump();
-
-	cout << "test() finished! \n";
 }
 
 
 int main()
 {
-    const int n = 2000;
-    Allocator alloc = Allocator(n);
 	test();
-	//cout << alloc.getBlockHeaderSize() ;
-	//getchar();
 	return 0;
 }
