229981a62ea24b8f007b08d0d8f7c536b81293b2
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-26 23:02:47 +0200

    fill table

--- PostgreSQL/WITH_RECURSIVE.sql
+++ PostgreSQL/WITH_RECURSIVE.sql
@@ -3,3 +3,17 @@ CREATE TABLE convertedPrograms (
   program_id bigint references programs("program_id"),
   program_parent_id bigint references programs("program_id")
 );
+
+INSERT
+INTO convertedPrograms (program_id, program_parent_id)
+VALUES
+  (11, 10),
+  (12, 10),
+  (13, 10),
+  (14, 10),
+  (15, 11),
+  (16, 11),
+  (17, 15),
+  (18, 15),
+  (19, 11),
+  (20, 10);
