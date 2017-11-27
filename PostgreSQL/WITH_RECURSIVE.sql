374d9121a3c25f6932a100895d964ddd42b9c549
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-27 02:02:29 +0200

    processing of cycles

--- PostgreSQL/WITH_RECURSIVE.sql
+++ PostgreSQL/WITH_RECURSIVE.sql
@@ -17,6 +17,7 @@ VALUES
   (18, 15),
   (19, 11),
   (20, 10);
+  -- (11, 17);
 
 
 -- Output children by parent ID
@@ -55,5 +56,68 @@ PREPARE getParentsByChildID (int) AS
   SELECT * FROM r;
 
 EXECUTE getParentsByChildID(17);
+
+
+-- Output children by parent ID with level
+PREPARE getChildrenByParentIDWithLevel (int) AS
+  WITH RECURSIVE r AS (
+    SELECT "program_id", 1 AS level
+    FROM programs
+    JOIN convertedPrograms
+    USING("program_id")
+    WHERE program_parent_id = $1
+    UNION
+    SELECT programs.program_id, r.level + 1 AS level
+    FROM programs
+    JOIN convertedPrograms
+      ON programs.program_id = convertedPrograms.program_id
+    JOIN r
+      ON convertedPrograms.program_parent_id = r.program_id
+  )
+  SELECT * FROM r;
+    
+EXECUTE getChildrenByParentIDWithLevel(10);
+
+
+-- add cycle
+INSERT
+INTO convertedPrograms (program_id, program_parent_id)
+VALUES
+  (11, 17);
+
+
+PREPARE getChildrenByParentID (int) AS
+  WITH RECURSIVE r AS (
+    SELECT
+      "program_id",
+      "program_parent_id",
+      1 AS level,
+      0 AS is_cycle
+    FROM programs
+    JOIN convertedPrograms
+    USING("program_id")
+    WHERE program_parent_id = $1
+    UNION
+    SELECT
+      programs.program_id,
+      convertedPrograms.program_parent_id,
+      r.level + 1 AS level,
+      (CASE
+        WHEN r.program_id = convertedPrograms.program_parent_id
+        AND r.program_parent_id = convertedPrograms.program_id 
+        THEN 1
+        ELSE 0
+        END
+      ) AS is_cycle
+    FROM programs
+    JOIN convertedPrograms
+      ON programs.program_id = convertedPrograms.program_id
+    JOIN r
+      ON convertedPrograms.program_parent_id = r.program_id
+    WHERE r.is_cycle = 0
+  )
+  SELECT * FROM r;
+
+EXECUTE getChildrenByParentID(10);
 
 
