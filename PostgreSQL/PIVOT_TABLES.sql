2e0c074c712d01eb4383d2ca3e5334ba1e2ba9bb
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-06 02:38:49 +0200

    add function with crosstab and multiple agr functions

--- PostgreSQL/PIVOT_TABLES.sql
+++ PostgreSQL/PIVOT_TABLES.sql
@@ -45,3 +45,105 @@ SELECT * FROM crosstab(
   "Nov" int,
   "Dec" int
 );
+
+
+
+with contracts_max_month_quantity as (
+  (SELECT * FROM crosstab(
+    'SELECT 
+      year,
+      month,
+      max(quantity)
+    FROM contracts
+    GROUP BY
+      year,
+      month,
+      quantity
+    ORDER BY 1',
+    'SELECT m FROM generate_series(1,12) m'
+  ) AS (
+    year int,
+    "Jan" int,
+    "Feb" int,
+    "Mar" int,
+    "Apr" int,
+    "May" int,
+    "Jun" int,
+    "Jul" int,
+    "Aug" int,
+    "Sep" int,
+    "Oct" int,
+    "Nov" int,
+    "Dec" int
+  ))
+)
+SELECT * FROM contracts_max_month_quantity;
+
+
+with contracts_max_month_quantity as (
+  (SELECT * FROM crosstab(
+    'SELECT 
+      year,
+      month,
+      max(quantity)
+    FROM contracts
+    GROUP BY
+      year,
+      month,
+      quantity
+    ORDER BY 1',
+    'SELECT m FROM generate_series(1,12) m'
+  ) AS (
+    year int,
+    "Jan" int,
+    "Feb" int,
+    "Mar" int,
+    "Apr" int,
+    "May" int,
+    "Jun" int,
+    "Jul" int,
+    "Aug" int,
+    "Sep" int,
+    "Oct" int,
+    "Nov" int,
+    "Dec" int
+  ))
+), contracts_avr_month_price as (
+  (SELECT * FROM crosstab(
+    'SELECT 
+      year,
+      month,
+      (avg(price))::int AS price
+    FROM contracts
+    JOIN programs
+    USING(program_id)
+    GROUP BY
+      year,
+      month,
+      price
+    ORDER BY 1',
+    'SELECT m FROM generate_series(1,12) m'
+  ) AS (
+    year int,
+    "Jan" int,
+    "Feb" int,
+    "Mar" int,
+    "Apr" int,
+    "May" int,
+    "Jun" int,
+    "Jul" int,
+    "Aug" int,
+    "Sep" int,
+    "Oct" int,
+    "Nov" int,
+    "Dec" int
+  ))
+)
+SELECT
+  year,
+  mq."Jan" AS "Jan MaxQnt", mp."Jan" AS "Jan AvgPrice",
+  mq."Feb" AS "Feb MaxQnt", mp."Feb" AS "Feb AvgPrice",
+  mq."Mar" AS "Mar MaxQnt", mp."Mar" AS "Mar AvgPrice",
+FROM contracts_max_month_quantity mq
+JOIN contracts_avr_month_price mp
+USING(year);
