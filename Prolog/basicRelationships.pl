daa3bb502d792ea6ceca52a1bed95ed3d6695327
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-09 15:28:40 +0200

    complete

--- Prolog/basicRelationships.pl
+++ Prolog/basicRelationships.pl
@@ -36,7 +36,56 @@ mother('Oksana', 'Roman').
 mother('Oksana', 'Peter').
 
 parent(X, Y) :- father(X, Y); mother(X, Y).
+child(X, Y) :- father(Y, X); mother(Y, X).
 
 son(X, Y) :- parent(Y, X), male(X).
 
 daughter(X, Y) :- parent(Y, X), female(X).
+
+brother(X, Y) :- parent(Z, X), parent(Z, Y), male(X).
+%% brother('Alexey', 'Yevgeny').
+
+sister(X, Y) :- parent(Z, X), parent(Z, Y), female(X).
+%% sister('Marina', 'Natalia').
+
+uncle(X, Y) :- brother(X, Z), parent(Z, Y).
+
+aunt(X, Y) :- sister(X, Z), parent(Z, Y).
+
+nephew(X, Y) :- uncle(Y, X), male(X); aunt(Y, X), male(X).
+
+niece(X, Y) :- uncle(Y, X), female(X); aunt(Y, X), female(X).
+
+grandfather(X, Y) :- parent(X, Z), child(Y, Z), male(X).
+%% grandfather('Kolya', 'Roman').
+
+grandmother(X, Y) :- parent(X, Z), child(Y, Z), female(X).
+
+grandson(X, Y) :- grandfather(Y, X), grandmother(Y, X), male(X).
+%% grandson('Roman', 'Kolya').
+
+granddaughter(X, Y) :- grandfather(Y, X), grandmother(Y, X), female(X).
+
+married(X, Y) :- child(Z, X), child(Z, Y).
+
+hismotherinlaw(X, Y) :- married(Z, Y), daughter(Z, X), mother(X, Z).
+
+hisfatherinlaw(X, Y) :- married(Z, Y), daughter(Z,X), father(X, Z).
+
+hermotherinlaw(X, Y) :- married(Z, Y), son(Z, X), mother(X, Z).
+
+herfatherinlaw(X, Y) :- son(Z, X), married(Z,Y), father(X, Z).
+
+soninlaw(X, Y) :- married(X, Z), father(Z,Y).
+
+daughterinlaw(X, Y) :- married(X, Z), son(Z, Y).
+
+brotherinlaw(X, Y) :- married(X, Z), sister(Z, D), married(D, Y).
+
+sisterinlaw(X, Y) :- sister(X, Z), married(Z, Y), sister(Z, X).
+
+diver(X, Y) :- married(Y, Z), brother(X, Z).
+
+greatnephew(X, Y) :- grandson(X, Z), parent(Q, Z), parent(Q, Y).
+
+greatniece(X, Y) :- granddaughter(X, Z), parent(Q, Z), parent(Q, Y).
