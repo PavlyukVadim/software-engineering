369b1d4ef15e71cd48699a99ce8dde805fe7664c
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-20 08:08:12 +0200

    add mapReduce

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -142,3 +142,82 @@ db.getCollection('contracts').aggregate([
     }
   }
 ])
+
+
+var mapComp = function () {
+  var output = {
+    MUid: this.MUId,
+    name: this.Nm,
+    MU: null
+  };
+  emit(this.MUId, output);
+};
+
+var mapCMU = function () {
+  var output= {
+    MUid: this.Id,
+    name:null,
+    MU:this.Nm
+  }
+  emit(this.Id, output);
+};
+
+var reduceF = function(key, values) {
+  var outs = {
+    name: null,
+    MU: null
+  };
+  values.forEach(function(v) {
+  if(outs.name == null) {
+    outs.name = v.name
+  }
+  if(outs.MU == null) {
+    outs.MU = v.MU
+  }
+  });
+  return outs;
+};
+
+result = db.cComp.mapReduce(mapComp, reduceF, {out: {reduce: 'Comp_CMU'}});
+result = db.CMU.mapReduce(mapCMU, reduceF, {out: {reduce: 'Comp_CMU'}});
+db.Comp_CMU.find()
+----------
+
+function mapPrograms() {
+  var result = {
+    pId: this.program_id,
+    programName: this.program_name,
+    contractId: null,
+  };
+  emit(result.pId, result);
+};
+
+function mapContracts() {
+  var result = {
+    pId: this.program_id,
+    programName: null,
+    contractId: this.contract_id, 
+  };
+  emit(result.pId, result);
+};
+
+function reduce(key, values) {
+  var result = {
+    contractId: null,
+    programName: null
+  };
+
+  values.forEach(function(obj) {
+    if (result.contractId == null) {
+      result.contractId = obj.contractId;
+    }
+    if (result.programName == null) {
+      result.programName = obj.programName;
+    }
+  });
+  return result;
+};
+
+result = db.getCollection('contracts').mapReduce(mapContracts, reduce, {out: {reduce: 'contractsWP'}});
+result = db.getCollection('programs').mapReduce(mapPrograms, reduce, {out: {reduce: 'contractsWP'}});
+db.getCollection('contractsWP').find({});
