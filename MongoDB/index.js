c48edeff26ee4fa4b1efa0b5ee224601c0f6a9c7
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-20 07:04:18 +0200

    add aggregate functions

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -82,3 +82,63 @@ db.getCollection('contracts').update(
     }
   }
 );
+
+
+// get number of contracts for each year
+db.getCollection('contracts').aggregate([
+  {
+    $group: {
+     _id: "$year",
+      count: { $sum: 1 }
+    }
+  }
+]);
+
+// get number of contracts after 2007
+db.getCollection('contracts').aggregate([
+  {
+    $match: {
+      year: {
+        $gt: 2007
+      }
+    },
+  },
+  {
+    $group: {
+     _id: "$year",
+      count: { $sum: 1 }
+    }
+  }
+]);
+
+// get quantity of client with the same name 
+db.getCollection('contracts').aggregate([
+  { 
+    $match: {
+      "client.fullName": {
+        $regex: /^A/
+      }
+    }
+  },
+  {
+    $group: {
+     _id: "$client.fullName" ,
+      count: { $sum: 1 },
+      quantity: { $sum: "$quantity" }
+    }
+  }
+]);
+
+// get avg quantity for each client by years
+db.getCollection('contracts').aggregate([
+  {
+    $group: {
+     _id: {
+         year: "$year",
+         client: "$client.fullName"
+      },
+      avg_quantity: { $avg: "$quantity" },
+      quantity: { $sum: "$quantity" }
+    }
+  }
+])
