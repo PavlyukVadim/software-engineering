91434f9827c4fa22aa6f2d0593ebfcdb5772ef44
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-05-16 10:57:10 +0300

    add alertDialog

--- Mobile(Android)/Select/app/src/main/java/com/example/amadev/select/MainActivity.java
+++ Mobile(Android)/Select/app/src/main/java/com/example/amadev/select/MainActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
-import java.util.List;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    final Context context = this;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,5 +63,38 @@ public class MainActivity extends AppCompatActivity {
 
         PersonListAdapter adapter = new PersonListAdapter(this, R.layout.adapter_view_layout, peopleList);
         mListView.setAdapter(adapter);
+
+        button = (Button) findViewById(R.id.buttonOk);
+
+        // add button listener
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set title
+                alertDialogBuilder.setTitle("Your Title");
+
+                // set dialog message
+                alertDialogBuilder
+                    .setMessage("Click yes to exit!")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // the dialog box and do nothing
+                            dialog.cancel();
+                        }
+                    });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }
+        });
     }
 }
