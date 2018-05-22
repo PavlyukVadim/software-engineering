aa000103b6ed263e9c1d283689dd0c26296fd707
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-05-23 00:36:49 +0300

    add fragments

--- Mobile(Android)/Select(Fragments)/app/src/main/java/com/example/amadev/select/Fragment1.java
+++ Mobile(Android)/Select(Fragments)/app/src/main/java/com/example/amadev/select/Fragment1.java
@@ -11,6 +11,6 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2, null);
+        return inflater.inflate(R.layout.fragment1, container, false);
     }
 }
--- Mobile(Android)/Select(Fragments)/app/src/main/java/com/example/amadev/select/Fragment2.java
+++ Mobile(Android)/Select(Fragments)/app/src/main/java/com/example/amadev/select/Fragment2.java
@@ -11,6 +11,6 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, null);
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
