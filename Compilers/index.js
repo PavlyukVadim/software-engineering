fd4606a12c175f9373976999a4c8d1315e854e63
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-03-27 12:09:44 +0200

    separate examples

--- Compilers/index.js
+++ Compilers/index.js
@@ -516,30 +516,3 @@ function parse(input) {
 //       evaluate(ast, globalEnv);
 //   });
 // })();
-
-const ts = TokenStream(InputStream('1 + 2'))
-console.log('ts', ts)
-
-// var ast1 = parse(TokenStream(InputStream('1 + 2')));
-var ast2 = parse(TokenStream(InputStream(`
-  var a = [
-    b: 45,
-    c: 71,
-  ];
-  #var a = {
-  #  foo: 'bar',
-  #};
-  # var b = 5;
-  # if(a <= b) {
-  #  return true;
-  # }
-`)));
-
-// console.log('ast1', JSON.stringify(ast1, null, 2))
-console.log('ast2', JSON.stringify(ast2, null, 2))
-
-// var ast1 = parse(TokenStream(InputStream('1 + 2')))
-
-// const ast1 = parse(TokenStream(InputStream('f(1, 3 + 4, 5)')))
-// const ast1 = parse(TokenStream(InputStream('(1 + 3)')))
-// console.log('ast1', JSON.stringify(ast1, null, 2))
