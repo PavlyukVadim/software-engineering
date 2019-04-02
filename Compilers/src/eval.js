ae7c92f9dde42a92f8a60a7c9b9c397245684a70
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-04-02 10:34:25 +0300

    refactoring

--- Compilers/src/eval.js
+++ Compilers/src/eval.js
@@ -2,17 +2,46 @@ const globalVars = {
   print: console.log,
 }
 
-function Environment(parent) {
-  this.vars = Object.create(parent ? parent.vars : null)
-  if (parent) {
-    this.parent = parent
+class Environment {
+  constructor(parent) {
+    this.vars = Object.create(parent ? parent.vars : null)
+    if (parent) {
+      this.parent = parent
+    }
+  }
+
+  lookup(name) {
+    let scope = this
+    while (scope) {
+      if (Object.prototype.hasOwnProperty.call(scope.vars, name)) {
+        return scope
+      }
+      scope = scope.parent
+    }
+  }
+
+  getVarValue(name) {
+    if (name in this.vars) {
+      return this.vars[name]
+      // TODO: refactor
+    } else if (this.parent && name in this.parent.vars) {
+      return this.parent.vars[name]
+    } else if (name in globalVars) {
+      return globalVars[name]
+    }
+    throw new Error('Undefined variable ' + name)
+  }
+
+  setVarValue(name, value) {
+    const scope = this.lookup(name)
+    if (!scope && this.parent) {
+      throw new Error('Undefined variable ' + name)
+    }
+    return (scope || this).vars[name] = value
   }
 }
 
 Environment.prototype = {
-  extend: function() {
-    return new Environment(this)
-  },
   lookup: function(name) {
     let scope = this
     while (scope) {
@@ -22,7 +51,7 @@ Environment.prototype = {
       scope = scope.parent
     }
   },
-  get: function(name) {
+  getVarValue: function(name) {
     if (name in this.vars) {
       return this.vars[name]
       // TODO: refactor
@@ -33,18 +62,16 @@ Environment.prototype = {
     }
     throw new Error("Undefined variable " + name)
   },
-  set: function(name, value) {
+  setVarValue: function(name, value) {
     const scope = this.lookup(name)
     if (!scope && this.parent) {
       throw new Error("Undefined variable " + name)
     }
     return (scope || this).vars[name] = value
   },
-  def: function(name, value) {
-    return this.vars[name] = value
-  },
 }
 
+  // name  // key                             // identifier
 const expTypes = {
   assign: 'assignExpression',                 // =
   objLiteral: 'objLiteral',                   // []
@@ -54,6 +81,8 @@ const expTypes = {
   binary: 'binaryExpression',                 // +, -, /, *, >, <
   loop: 'forEachStatement',                   // forEach,
   loopVar: 'identifierExpression',            // as
+  prog: 'prog',                               // { ... }
+  func: 'call',                               // ()
 }
 
 const varTypes = [
@@ -66,54 +95,21 @@ function evaluate(exp, env) {
   switch (exp.type) {
     case 'num':
     case 'str':
-    case 'bool':
-      return exp.value
-
-    case 'var':
-      return env.get(exp.value)
-
-    case 'testVar':
-      return env.get(exp.name)
+    case 'bool': return exp.value
+    case 'var': return env.getVarValue(exp.value)
+    case 'testVar': return env.getVarValue(exp.name)
 
     case expTypes.assign: {
       if (!varTypes.includes(exp.left.type)) {
         throw new Error('Cannot assign to ' + JSON.stringify(exp.left))
       }
 
-      const value = evaluate(exp.right, env)
+      let value = evaluate(exp.right, env)
       const varType = exp.left.type
       const varName = exp.left.name
-      if (varType === 'testVar') {
-        value.questions = {
-          items: [],
-          add: (newQuestion) => {
-            value.questions.items.push(newQuestion)
-          },
-          remove: (wasteQuestion) => {
-            value.questions.items = value.questions.items.filter((item) => (
-              item.description !== wasteQuestion.description
-            ))
-          },
-          get amount() {
-            return value.questions.items.length
-          }
-        }
-        value.answers = {
-          items: [],
-          add: (newQuestion) => {
-            value.answers.items.push(newQuestion)
-          },
-          remove: (wasteQuestion) => {
-            value.answers.items = value.answers.items.filter((item) => (
-              item.id !== wasteQuestion.id
-            ))
-          },
-          get amount() {
-            return value.answers.items.length
-          }
-        }
-      }
-      return env.set(varName, value)
+
+      value = (varType === 'testVar') ? testFactory(value) : value
+      return env.setVarValue(varName, value)
     }
 
     case expTypes.objLiteral: {
@@ -126,11 +122,7 @@ function evaluate(exp, env) {
     }
 
     case expTypes.literalAssign: {
-      if (
-        !exp.left ||
-        !varTypes.includes(exp.left.type)
-      ) {
-        console.log('exp', exp)
+      if (!exp.left || !varTypes.includes(exp.left.type)) {
         throw new Error('Cannot assign to ' + JSON.stringify(exp.left))
       }
 
@@ -140,27 +132,15 @@ function evaluate(exp, env) {
     }
 
     case expTypes.member: {
+      // create a inner env that includes current scope as parent
+      const innerEnv = new Environment(env)
       // get all members values of the literal
       const parent = evaluate(exp.left, env)
-      // const localEnv = {
-      //   vars: {
-      //     ...parent,
-      //   },
-      // }
-      // create a new env that consist only from parent members
-      const innerEnv = new Environment(env)
       innerEnv.vars = {
         ...parent,
       }
-      // call child on a closed env
-
-      // let child
-      // if (exp.right.type === 'call') {
-      //   child = evaluate(exp.right, globalEnv)
-      // } else {
+      // call child on a inner env
       const child = evaluate(exp.right, innerEnv)
-      // }
-      
       return child
     }
 
@@ -171,9 +151,6 @@ function evaluate(exp, env) {
         evaluate(exp.right, env)
       )
     }
-      
-    // case "lambda":
-    //   return make_lambda(env, exp);
 
     case expTypes.cond: {
       const cond = evaluate(exp.cond, env)
@@ -189,7 +166,7 @@ function evaluate(exp, env) {
     case expTypes.loop: {
       const [varName, varValues] = evaluate(exp.inner, env)
       varValues.forEach((varValue) => {
-        // create a new env that consist only from parent members
+        // create a inner env that includes current scope as parent
         const innerEnv = new Environment(env)
         innerEnv.vars = {
           [varName]: varValue,
@@ -205,15 +182,16 @@ function evaluate(exp, env) {
       return [varName, value]
     }
 
-    case "prog":
+    case expTypes.prog: {
       let val = false
-      exp.prog.forEach(function(exp) {
+      exp.prog.forEach((exp) => {
         val = evaluate(exp, env)
       })
       return val
+    }
 
-    case 'call': {
-      var func = evaluate(exp.func, env)
+    case expTypes.func: {
+      const func = evaluate(exp.func, env)
       return func.apply(null, exp.args.map((arg) => {
         return evaluate(arg, env)
       }))
@@ -252,51 +230,23 @@ function applyOp(op, a, b) {
   throw new Error('Can\'t apply operator ' + op)
 }
 
-// function make_lambda(env, exp) {
-//   function lambda() {
-//       var names = exp.vars;
-//       var scope = env.extend();
-//       for (var i = 0; i < names.length; ++i)
-//           scope.def(names[i], i < arguments.length ? arguments[i] : false);
-//       return evaluate(exp.body, scope);
-//   }
-//   return lambda;
-// }
-
-
-
-
-// /* -----[ entry point for NodeJS ]----- */
-
-// var globalEnv = new Environment();
-
-
-
-// globalEnv.def("time", function(func){
-//   try {
-//       console.time("time");
-//       return func();
-//   } finally {
-//       console.timeEnd("time");
-//   }
-// });
-
-// if (typeof process != "undefined") (function(){
-//   var util = require("util");
-//   globalEnv.def("println", function(val){
-//       util.puts(val);
-//   });
-//   globalEnv.def("print", function(val){
-//       util.print(val);
-//   });
-//   var code = "";
-//   process.stdin.setEncoding("utf8");
-//   process.stdin.on("readable", function(){
-//       var chunk = process.stdin.read();
-//       if (chunk) code += chunk;
-//   });
-//   process.stdin.on("end", function(){
-//       var ast = parse(TokenStream(InputStream(code)));
-//       evaluate(ast, globalEnv);
-//   });
-// })();
+function testFactory(test) {
+  const props = ['questions', 'answers']
+  props.forEach((prop) => {
+    test[prop] = {
+      items: [],
+      add: (newItem) => {
+        test[prop].items.push(newItem)
+      },
+      remove: (wasteItem) => {
+        test[prop].items = test[prop].items.filter((item) => (
+          item.description !== wasteItem.description
+        ))
+      },
+      get amount() {
+        return test[prop].items.length
+      }
+    }
+  })
+  return test
+}
